41b41c5bec02b4adb31074b125aa6fce290d8157
AdelinaKh <90128968+AdelinaKh@users.noreply.github.com>
2022-01-23 23:16:45 +0300

    prj8_remove to src

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 module.exports = {
-  entry: { main: './scripts/index.js' },
+  entry: { main: './src/scripts/index.js' },
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'main.js'
@@ -50,7 +50,7 @@ module.exports = {
     ]
   },
   plugins: [
-    new HtmlWebpackPlugin({template: './index.html'}),
+    new HtmlWebpackPlugin({template: './src/pages/index.html'}),
     new MiniCssExtractPlugin(),
     new CleanWebpackPlugin(), 
   ]
